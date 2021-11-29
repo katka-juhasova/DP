e3153a15b1dbdb2d610dc169dc60d6c5ba1d3e20
katka-juhasova <katkajuhasova8@gmail.com>
2021-11-29 22:32:05 +0100

    Update PointNet for transfer learning

--- pointnet/pointnet.py
+++ pointnet/pointnet.py
@@ -8,16 +8,16 @@ NUM_CLASSES = 40
 BATCH_SIZE = 32
 
 
-def conv1d_bn(x, filters):
+def conv1d_bn(x, filters, name=None):
     x = layers.Conv1D(filters, kernel_size=1, padding="valid")(x)
     x = layers.BatchNormalization(momentum=0.9)(x)
-    return layers.Activation("relu")(x)
+    return layers.Activation("relu", name=name)(x)
 
 
-def dense_bn(x, filters):
+def dense_bn(x, filters, name=None):
     x = layers.Dense(filters)(x)
     x = layers.BatchNormalization(momentum=0.9)(x)
-    return layers.Activation("relu")(x)
+    return layers.Activation("relu", name=name)(x)
 
 
 class OrthogonalRegularizer(keras.regularizers.Regularizer):
@@ -34,12 +34,12 @@ class OrthogonalRegularizer(keras.regularizers.Regularizer):
 
     def get_config(self):
         return {
-            'num_features': self.num_features,
-            'l2reg': self.l2reg
+            "num_features": self.num_features,
+            "l2reg": self.l2reg
         }
 
 
-def tnet(inputs, num_features, regularize=False):
+def tnet(inputs, num_features, regularize=False, name=None):
     # Initialise bias as the identity matrix
     bias = keras.initializers.Constant(np.eye(num_features).flatten())
 
@@ -61,25 +61,31 @@ def tnet(inputs, num_features, regularize=False):
     feat_t = layers.Reshape((num_features, num_features))(x)
 
     # Apply affine transformation to input features
-    return layers.Dot(axes=(2, 1))([inputs, feat_t])
+    return layers.Dot(axes=(2, 1), name=name)([inputs, feat_t])
 
 
-def get_model(num_points=NUM_POINTS, num_classes=NUM_CLASSES, name='pointnet'):
-    inputs = keras.Input(shape=(num_points, 3))
+def get_model(num_points=NUM_POINTS, num_classes=NUM_CLASSES,
+              name="pointnet", weights=None):
+
+    inputs = keras.Input(shape=(num_points, 3), name=name + "_input")
 
     x = tnet(inputs, 3, regularize=False)
     x = conv1d_bn(x, 64)
     x = conv1d_bn(x, 64)
-    x = tnet(x, 64, regularize=True)
+    x = tnet(x, 64, regularize=True, name=name + "_local_features")
     x = conv1d_bn(x, 64)
     x = conv1d_bn(x, 128)
     x = conv1d_bn(x, 1024)
-    x = layers.GlobalMaxPooling1D()(x)
+    x = layers.GlobalMaxPooling1D(name=name + "_global_features")(x)
     x = dense_bn(x, 512)
     x = layers.Dropout(0.3)(x)
     x = dense_bn(x, 256)
     x = layers.Dropout(0.3)(x)
 
     outputs = layers.Dense(num_classes, activation="softmax")(x)
+    model = keras.Model(inputs=inputs, outputs=outputs, name=name)
 
-    return keras.Model(inputs=inputs, outputs=outputs, name=name)
+    if weights:
+        model.load_weights(weights)
+
+    return model
